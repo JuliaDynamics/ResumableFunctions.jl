3529455d7329135d33d6f6b4b35efabe28fe356a
Ben Lauwens <ben.lauwens@gmail.com>
2017-08-18 00:01:42 +0200

    Add getslots

--- src/macro.jl
+++ src/macro.jl
@@ -2,10 +2,13 @@ using MacroTools: postwalk, striplines, flatten, unresolve, resyntax
 
 macro resumable(expr::Expr)
   expr.head != :function && error("Expression is not a function definition!")
-  func = splitdef(expr)
-  println(func[:name])
+  func_def = splitdef(expr)
+  
   new_expr = postwalk(transform_for, expr)
   new_expr = new_expr |> striplines |> flatten |> unresolve |> resyntax
   println(new_expr)
+  slots = getslots(new_expr, func_def[:name])
+  println(slots)
+  type_name = gensym()
   esc(new_expr)
 end
--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,18 @@
+function getslots(expr::Expr, name::Symbol) :: Dict{Symbol, Type}
+  slots = Dict{Symbol, Type}()
+  eval(expr)
+  code_data_infos = @eval code_typed($name)
+  for (code_info, data_type) in code_data_infos
+    for i in 2:length(code_info.slotnames)
+      slots[code_info.slotnames[i]] = code_info.slottypes[i]
+    end
+  end
+  delete!(slots, Symbol("#temp#"))
+  delete!(slots, Symbol("#unused#"))
+  slots
+end
+
+
 # Copy of utility functions from MacroTools (master)
 # Can be removed once new version of MacroTools is released
 
