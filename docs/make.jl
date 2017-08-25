9694114c2857c2a7a1920664a6a788b05a5194f1
Ben Lauwens <ben.lauwens@gmail.com>
2017-08-25 16:05:32 +0200

    Some more documentation changes

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,14 @@
 using Documenter
 using ResumableFunctions
 
-makedocs()
+makedocs(
+  format = :html,
+  sitename = "ResumableFunctions",
+  pages = [
+      "index.md",
+  ]
+)
+
 
 deploydocs(
   repo = "github.com/benlauwens/ResumableFunctions.jl.git"
