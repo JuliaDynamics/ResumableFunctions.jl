17777f3a691a6477954f01da326af05f3dab3b4e
Ben Lauwens <ben.lauwens@gmail.com>
2017-08-25 21:23:35 +0200

    Make docs build

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,7 @@ makedocs(
 )
 
 deploydocs(
-  repo = "github.com/benlauwens/ResumableFunctions.jl.git"
+  repo = "github.com/benlauwens/ResumableFunctions.jl.git",
   target = "build",
   deps   = nothing,
   make   = nothing
