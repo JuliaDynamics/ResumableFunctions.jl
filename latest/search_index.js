cd1882f56b36c4db661916b6c7d13fc09177b878
autodocs <autodocs>
2017-08-25 20:33:45 +0000

    build based on 56f39cb

--- latest/search_index.js
+++ latest/search_index.js
@@ -2,16 +2,64 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "index.html#",
-    "page": "ResumableFunctions",
-    "title": "ResumableFunctions",
+    "page": "Home",
+    "title": "Home",
     "category": "page",
     "text": ""
 },
 
 {
     "location": "index.html#ResumableFunctions-1",
-    "page": "ResumableFunctions",
+    "page": "Home",
     "title": "ResumableFunctions",
+    "category": "section",
+    "text": "C# sharp style generators a.k.a. semi-coroutines for Julia."
+},
+
+{
+    "location": "manual.html#",
+    "page": "Manual",
+    "title": "Manual",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "manual.html#Manual-1",
+    "page": "Manual",
+    "title": "Manual",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "internals.html#",
+    "page": "Internals",
+    "title": "Internals",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "internals.html#Internals-1",
+    "page": "Internals",
+    "title": "Internals",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "library.html#",
+    "page": "Library",
+    "title": "Library",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "library.html#Library-1",
+    "page": "Library",
+    "title": "Library",
     "category": "section",
     "text": ""
 },
