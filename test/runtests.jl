7d00abc4a28b9012faaa9be8127e2383428de109
Gabriel Gerlero <gerlero@users.noreply.github.com>
2022-11-29 02:15:38 -0300

    Add two-way yield_from test case

--- test/runtests.jl
+++ test/runtests.jl
@@ -160,3 +160,25 @@ end
 @testset "test_yield_from" begin
 @test collect(test_yield_from(4)) == [42, 314, 1, 4, 9, 16, 4, 1, 4, 9, 16, 25]
 end
+
+@resumable function test_echo()
+  x = 0
+  while true
+    x = @yield x
+  end
+  return "Done"
+end
+
+@resumable function test_forward()
+  ret = @yield_from test_echo()
+  @test ret == "Done"
+  @yield_from test_echo()
+end
+
+@testset "test_yield_from_twoway" begin
+forward = test_forward()
+@test forward() == 0
+for i in 1:5
+  @test forward(i) == i
+end
+end
