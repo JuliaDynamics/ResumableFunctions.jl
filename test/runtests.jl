2a1c335c44e3f5cf05cd98a01ffd17a5086832ce
Luca Di Gaspero <l.digaspero@uniud.it>
2023-01-21 13:41:50 +0100

    Added a further test for optional type parmeters.

--- test/runtests.jl
+++ test/runtests.jl
@@ -159,3 +159,27 @@ end
   @test collect(test_type_parameters(Float64, 1)) == [1.0, 2.0, 3.0, 4.0]
 end
 
+@resumable function test_type_parameters_order(start::Int64, ::Type{T})::T where {T}
+  for i in start:start+3
+    @yield T(i)
+  end
+end
+
+@testset "test_type_parameters_order" begin
+  @test collect(test_type_parameters_order(1, Int64)) == [1, 2, 3, 4]
+  @test collect(test_type_parameters_order(1, Float64)) == [1.0, 2.0, 3.0, 4.0]  
+  @test typeof(first(test_type_parameters_order(1, ComplexF32))) == ComplexF32
+end
+
+@resumable function test_type_parameters_optional(start::Int64; t::Type{T}=Type{Int64})::T where {T}
+  for i in start:start+3
+    @yield T(i)
+  end
+end
+
+
+@testset "test_type_parameters_optional" begin
+  @test collect(test_type_parameters_optional(1, t=Int64)) == [1, 2, 3, 4]
+  @test collect(test_type_parameters_optional(1, t=Float64)) == [1.0, 2.0, 3.0, 4.0]  
+  @test typeof(first(test_type_parameters_optional(1, t=ComplexF32))) == ComplexF32
+end
